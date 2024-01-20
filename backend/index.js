be89c4c72c86f20d29a6a0e135b8d3da169d4a54
Kushagra Agrawal <agrawalkushagra29@gmail.com>
2024-01-20 14:34:14 +0530

    Changed routes name

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const cors = require("cors");
+
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
