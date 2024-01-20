cb31f890cde696456b3f36ebc90fee8b5dd37416
Kushagra Agrawal <agrawalkushagra29@gmail.com>
2024-01-20 16:35:22 +0530

    commiting

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 
-const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
 const cookieParser = require("cookie-parser");
