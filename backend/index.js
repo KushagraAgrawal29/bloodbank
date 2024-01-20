97c3a975eaa58b8bf55f2f7fced87008d24d1a56
Kushagra Agrawal <agrawalkushagra29@gmail.com>
2024-01-20 14:40:35 +0530

    updating routes name

--- backend/index.js
+++ backend/index.js
@@ -7,4 +7,31 @@ const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 
 const app = express();
+const database = require("./config/database");
+
+const userRoutes = require("./routes/userRouter");
+const bankRoutes = require("./routes/bankRouter");
+const campRoutes = require("./routes/campRouter");
+const authRoutes = require("./routes/authRouter");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+
+//database connection
+database.connect();
+
+//middleware
+app.use(cookieParser());
+app.use(express.json());
+app.use(
+	cors({
+		origin: [
+			"http://localhost:3000",
+		],
+		credentials: true,
+	})
+);
+
 
