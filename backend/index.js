be50c430cfada1b73ab92534e257bcd5f3d91cac
Kushagra Agrawal <agrawalkushagra29@gmail.com>
2024-01-20 14:43:01 +0530

    changed route folder name and finalised index.js

--- backend/index.js
+++ backend/index.js
@@ -9,10 +9,10 @@ const cookieParser = require("cookie-parser");
 const app = express();
 const database = require("./config/database");
 
-const userRoutes = require("./routes/userRouter");
-const bankRoutes = require("./routes/bankRouter");
-const campRoutes = require("./routes/campRouter");
-const authRoutes = require("./routes/authRouter");
+const userRoutes = require("./routers/userRouter");
+const bankRoutes = require("./routers/bankRouter");
+const campRoutes = require("./routers/campRouter");
+const authRoutes = require("./routers/authRouter");
 
 const dotenv = require("dotenv");
 dotenv.config();
@@ -33,5 +33,22 @@ app.use(
 		credentials: true,
 	})
 );
+
+app.use("/auth",require("./routers/authRouter"));
+app.use("/user", require("./routers/userRouter"));
+app.use("/bank", require("./routers/bankRouter"));
+app.use("/camps", require("./routers/campRouter"));
+
+//default route
+app.get("/",(req,res) => {
+    return res.status(200).json({
+        success:true,
+        message:"Your server is up and running...",
+    });
+});
+
+app.listen(PORT,()=>{
+    console.log(`App is running at ${PORT}`)
+})
 
 
