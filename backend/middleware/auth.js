21e5eb34683da5f71fde92c88d0329f57c6107f3
Kushagra Agrawal <agrawalkushagra29@gmail.com>
2024-02-02 18:17:50 +0530

    some change

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,23 +1,22 @@
 const jwt = require("jsonwebtoken");
 
-function auth(request,response,next) {
+function auth(req,res,next) {
      try{
-        const token = request.cookies.token;
+        const token = req.cookies.token;
         if(!token){
-            return response.staus(401).json({
+            return res.status(401).json({
                 success:false,
                 message:"No token found",
             });
         }
 
         const verified = jwt.verify(token,process.env.JWT_SECRET);
-        request.user = verified.user;
-
+        req.user = verified.user;
         next();
      }
      catch(error){
         console.log(error);
-        return response.staus({
+        return res.status(500).json({
             success:false,
             message:"User is not authorised",
         });
