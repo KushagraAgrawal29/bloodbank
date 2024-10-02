8b373d2a090a3b5320777f1871b03d3e4ea11300
Kushagra Agrawal <151816380+KushagraAgrawal29@users.noreply.github.com>
2024-10-02 21:44:10 +0530

    some what change

--- client/src/Components/Auth/Auth.js
+++ client/src/Components/Auth/Auth.js
@@ -98,7 +98,7 @@ const Auth = (props) => {
     }
 
     await axios
-      .post(`/auth/${handle}`, formData, { withCredentials: true })
+      .post(`https://bloodbank-x7lu.vercel.app/auth/${handle}`, formData)
       .then(
         async (res) => {},
         (err) => alert(err.response.data.errorMessage)
